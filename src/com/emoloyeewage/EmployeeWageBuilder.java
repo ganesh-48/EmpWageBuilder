14f7b4aeef36a96ec3384b308a3558c915436ddf
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-28 09:13:23 +0530

    ADDED: Calculated Employee Wage for a month

--- src/com/emoloyeewage/EmployeeWageBuilder.java
+++ src/com/emoloyeewage/EmployeeWageBuilder.java
@@ -8,11 +8,14 @@ public class EmployeeWageBuilder {
 
 		int IS_PART_TIME = 1;
 		int IS_FULL_TIME = 2;
+		int TOTAL_SALARY = 0;
 		int EMP_RATE_PER_HRS = 20;
-		int salary , empHrs;
-		Random rand = new Random();
-		int empCheck = rand.nextInt(3);
-		switch(empCheck) {
+		int NUM_WORKING_DAYS = 20;
+		int salary, empHrs;
+		for (int day = 1; day < NUM_WORKING_DAYS; day++) {
+			Random rand = new Random();
+			int empCheck = rand.nextInt(3);
+			switch (empCheck) {
 			case 1:
 				empHrs = 8;
 				System.out.println("Employee is Present");
@@ -20,14 +23,16 @@ public class EmployeeWageBuilder {
 			case 2:
 				empHrs = 4;
 				System.out.println("Employee is part time");
-
 				break;
 			default:
-				empHrs= 0;
+				empHrs = 0;
 				System.out.println("Employee is Absent");
 				break;
+			}
+			salary = (empHrs * EMP_RATE_PER_HRS);
+			System.out.println("salary:" + salary);
+			TOTAL_SALARY = (TOTAL_SALARY + salary);
+			System.out.println("TOTAL_SALARY:" + TOTAL_SALARY);
 		}
-		salary=(empHrs * EMP_RATE_PER_HRS );
-		System.out.println("salary:"+salary);
 	}
 }
